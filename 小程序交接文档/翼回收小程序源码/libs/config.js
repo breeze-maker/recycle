4efb60b86f07795d7aece1fac5dc1d57fceb6e93
c245099212@126.com <c245099212@126.com>
2018-08-20 11:48:18 +0800

    删除代码

--- 小程序交接文档/翼回收小程序源码/libs/config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-  key: '58332c0da8f11a4bd9ed7eaa1df73d88'
-}
-
-module.exports.Config = config;
--- 小程序交接文档/翼回收小程序源码/pages/order/order.js
+++
@@ -1,8 +0,0 @@
-
-var common = require('../../common/common.js');
-Page({
-  onShareAppMessage(options) {
-    console.log(common);
-    console.log(options.webViewUrl)
-  }
-}) 
